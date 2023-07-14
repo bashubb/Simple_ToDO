49b7cc5a1cdb63b570b5463c97de25a3aa8372bb
Hubert Kielkowski <hubert.kielkowski@gmail.com>
2023-07-14 06:56:34 +0200

    Initial Commit

---
+++ Simple_ToDO/Simple_ToDOApp.swift
@@ -0,0 +1,17 @@
+//
+//  Simple_ToDOApp.swift
+//  Simple_ToDO
+//
+//  Created by HubertMac on 14/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct Simple_ToDOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
